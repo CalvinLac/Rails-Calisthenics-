d12928f6d8e4403d3dbd921bdecfcbe0418e66d9
Calvin <Calvin.lacc@gmail.com>
2016-08-23 15:03:23 -0600

    Finished creating the seeding file to populate the database, using the faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Category.destroy_all
+Post.destroy_all
+Tag.destroy_all
+Comment.destroy_all
+Tagging.destroy_all
+UserPost.destroy_all
+
+puts "user"
+10.times do 
+  u = User.new
+  u.name = Faker::Name.name
+  u.save
+end
+
+puts "cat"
+10.times do 
+  c = Category.new
+  c.name = Faker::Name.name
+  c.save
+end
+
+puts "post"
+10.times do 
+  p = Post.new
+  p.title = Faker::Lorem.word
+  p.body = Faker::Lorem.sentence
+  p.category_id = Category.all.sample.id
+  p.save
+end
+
+puts "tag"
+10.times do
+  t = Tag.new
+  t.name = Faker::Lorem.word
+  t.save
+end 
+
+puts "userpost"
+10.times do 
+  up = UserPost.new
+  up.user_id = User.all.sample.id
+  up.post_id = Post.all.sample.id
+  up.save
+end
+
+puts "taggings"
+10.times do 
+  ting = Tagging.new
+  ting.post_id = Post.all.sample.id
+  ting.tag_id = Tag.all.sample.id
+  ting.save
+end
+
+puts "comments"
+10.times do 
+  com = Comment.new 
+  com.body = Faker::Lorem.sentence
+  com.user_id = User.all.sample.id
+  com.post_id = Post.all.sample.id
+  com.save
+end 
+
+puts "seeding done, boi"
+
+
+
+
